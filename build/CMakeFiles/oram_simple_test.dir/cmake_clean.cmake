7448a87256d247894b3ec0d0c0eca7e3fe41adc4
Clive2312 <clivehaha@outlook.com>
2026-01-17 22:16:15 -0800

    claude step 1

---
+++ build/CMakeFiles/oram_simple_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/oram_simple_test.dir/tests/simple_test.cpp.o"
+  "CMakeFiles/oram_simple_test.dir/tests/simple_test.cpp.o.d"
+  "oram_simple_test"
+  "oram_simple_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/oram_simple_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
